da5ef7edc1833fe977c9af3aa3bd8808c4c9093c
fakesanghyeok <s24066@gsm.hs.kr>
2024-10-21 15:12:33 +0900

    홈화면 완성

---
+++ src/page/Home/Home.jsx
@@ -0,0 +1,12 @@
+import Header from "../../components/Header";
+import HomeBody from "./HomeBody";
+function Home(){
+    return(
+        <>
+            <Header />
+            <HomeBody />
+        </>
+    )
+}
+
+export default Home;
